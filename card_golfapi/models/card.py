5015444d88a9a917ada4b31b073707cb54693bb3
Dan Ross <daniellross@gmail.com>
2021-07-29 11:49:38 -0500

    finished models. added fixtures for cards

--- card_golfapi/models/card.py
+++ card_golfapi/models/card.py
@@ -4,4 +4,4 @@ class Card(models.Model):
     suite = models.CharField(max_length=15)
     name = models.CharField(max_length=15)
     score = models.IntegerField()
-    image = models.URLField()
+    image = models.URLField(blank=True)
