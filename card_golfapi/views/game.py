ebfd633e538eabb1d41a6a0681cdeea1f4ebb25a
Dan Ross <daniellross@gmail.com>
2021-07-29 14:51:08 -0500

    Added gametypes. started view on games

---
+++ card_golfapi/views/game.py
@@ -0,0 +1,5 @@
+"""View module for handling requests related to games"""
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
